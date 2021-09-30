18a557b2d476c844572ed94243e9de7f8d67c2c0
AmitKSwain <amit.here1@gmail.com>
2021-09-30 16:36:41 +0530

    1 file modified

--- routes/test.js
+++ routes/test.js
@@ -3,4 +3,4 @@ const router  = express.Router()
 const testController = require('../controllers/test')
 
 router.get("/api/test",testController.testMessege)
-modules.exports = router
+module.exports = router
