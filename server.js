f997613ad47c961cd0379a54f7e5dd53126e28c1
AmitKSwain <amit.here1@gmail.com>
2021-09-30 16:49:00 +0530

    1 file modified, 1 added

--- server.js
+++ server.js
@@ -1,23 +1,17 @@
-const express = require ('express');
+const express = require('express');
 const routes = require('./routes/test'); // import the routes
+const logger = require('./middlewares/logger');
 
 const app = express();
-var myLogger = function (req, res, next) {
-    console.log('LOGGED')
-    next()
-  }
-  var myLogger2 = function (req, res, next) {
-    console.log('LOGGED2')
-    next()
-  }
-app.use(myLogger)
-app.use(myLogger2)
+
+app.use(logger);
+
 app.use(express.json());
 
 app.use('/', routes);
 
 // process.env.PORT lets the port be set by Heroku
-let port = process.env.PORT || 8080
-app.listen(port, function() {
-    console.log('Our app is running on port :' + port);
-})
+let port = process.env.PORT || 8080;
+app.listen(port, function () {
+  console.log('Our app is running on port :' + port);
+});
