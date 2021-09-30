e90098b64f67d97d9ca33b3c46944ae14e8d481a
AmitKSwain <amit.here1@gmail.com>
2021-09-30 13:51:56 +0530

    2 files modified

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
-// run `node index.js` in the terminal
+var express = require('express');
+var app = express();
+app.use(express.json());
 
-console.log(`Hello Node.js v${process.versions.node}!`);
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+app.get("/api/test",(req,res)=>{
+  res.status(200).json("Hi")
+})
+
+
+app.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
+})
