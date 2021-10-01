e658772e081267dec36419d2535be0b1e95ed8b8
AmitKSwain <amit.here1@gmail.com>
2021-10-01 15:05:49 +0530

    1 file modified

--- routes/test.js
+++ routes/test.js
@@ -1,6 +1,15 @@
 const express = require('express')
 const router  = express.Router()
 const testController = require('../controllers/test')
+/**
+ * @swagger
+ * /api/test:
+ * get:
+ *     summery:This api is used to check if get method is working
+ *     responses:
+ *        200:
+ *           description: To test Get method
+ */
 
 router.get("/api/test",testController.testMessege)
 module.exports = router
