2a17dd0d4a090ac04078516ee81b757b8fbb5343
AmitKSwain <amit.here1@gmail.com>
2021-10-01 16:04:23 +0530

    1 file modified

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const swaggerUi = require('swagger-ui-express'),
 const app = express();
 app.use(express.json());
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use(logger);
 app.use('/', routes);
 
