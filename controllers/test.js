3ff92f460b5feb11eb9748633a222e230eb73a9b
AmitKSwain <amit.here1@gmail.com>
2021-10-01 16:20:43 +0530

    2 files modified

--- controllers/test.js
+++ controllers/test.js
@@ -1,7 +1,14 @@
+const swaggerSpec = require('./spec.json');
+
 const testMessege = (req,res,next) =>{
       res.status(200).json("Hello")
 }
 
+const specResponse = (req,res) =>{
+    res.status(200).json(swaggerSpec)
+}
+
 module.exports={
-    testMessege
+    testMessege,
+    specResponse
 }
--- routes/test.js
+++ routes/test.js
@@ -1,15 +1,8 @@
 const express = require('express')
 const router  = express.Router()
 const testController = require('../controllers/test')
-/**
- * @swagger
- * /api/test:
- * get:
- *     summery:This api is used to check if get method is working
- *     responses:
- *        200:
- *           description: To test Get method
- */
+
 
 router.get("/api/test",testController.testMessege)
+router.get("/",testController.specResponse)
 module.exports = router
