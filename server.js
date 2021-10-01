66edd426ed826d5559161667d2343cba74051317
AmitKSwain <amit.here1@gmail.com>
2021-10-01 14:43:02 +0530

    3 files modified

--- server.js
+++ server.js
@@ -1,10 +1,29 @@
 const express = require('express');
 const routes = require('./routes/test'); // import the routes
 const logger = require('./middlewares/logger');
+const swaggerJSDoc = require('swagger-jsdoc')
+const swaggerUi = require('swagger-ui-express')
 
 const app = express();
 app.use(express.json());
 
+const options= {
+  defination:{
+    openapi:'3.0.0',
+    info:{
+      title:'Apis for RRP webportal',
+      version:'1.0.0'
+    },
+    servers:[
+      {
+        api:'node-rvgxxv--8080.local.webcontainer.io'
+      }
+    ]
+  },
+  apis:['./server.js']
+}
+const swaggerSpec = swaggerJSDoc(options)
+app.use('/api-docs',swaggerUi.serve,swaggerUi.setup(swaggerSpec))
 app.use(logger);
 app.use('/', routes);
 
