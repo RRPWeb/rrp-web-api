156067e9fa59914ac31ecd889a587b9ec62dd5ad
AmitKSwain <amit.here1@gmail.com>
2021-10-05 21:43:28 +0530

    1 file modified

--- services/UserInfo.service.js
+++ services/UserInfo.service.js
@@ -4,6 +4,7 @@ exports.getAllUsers = async () => {
   let [data,err] = await userInfo.getUsers();
   if(err){
     console.log(err);
+    console.log(data)
     return "Error"
   }else{
     console.log(data)
