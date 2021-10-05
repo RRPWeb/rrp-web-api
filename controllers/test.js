5308bed8b84fef70d463dbefc94b9a12aca027e2
AmitKSwain <amit.here1@gmail.com>
2021-10-05 16:10:55 +0530

    2 files modified

--- controllers/test.js
+++ controllers/test.js
@@ -1,8 +1,9 @@
 
 const getUsers = require('../repository/UserInfo')
 const testMessege = (req,res,next) =>{
-      res.status(200).json(getUsers())
-     
+    getUsers()
+      res.status(200).json("Done")
+      
 }
 
 
