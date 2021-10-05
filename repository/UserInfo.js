7f3886c6ef0d374d8f4519829e3718f06259c656
AmitKSwain <amit.here1@gmail.com>
2021-10-05 21:18:47 +0530

    1 file modified

--- repository/UserInfo.js
+++ repository/UserInfo.js
@@ -6,7 +6,7 @@ exports.getUsers = async ()=>{
   console.log("Inside getUsers...")
    try{
     const result = await dbcon.query(query)
-    console.log("Result is : "+ JSON.Stringify(result[0]))
+    console.log("Result is : "+ Object.values(JSON.parse(JSON.stringify(result))))
     return "Success" 
    }catch(err){
      console.log(err)
