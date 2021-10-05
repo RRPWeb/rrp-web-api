807e6b8b112db316355bf1ff201f8f1166377ddf
AmitKSwain <amit.here1@gmail.com>
2021-10-05 15:00:22 +0530

    1 file modified

--- server.js
+++ server.js
@@ -6,10 +6,6 @@ const swaggerUi = require('swagger-ui-express');
 const apiSpec = YAML.load('spec.yml');
 const mysql = require("mysql");
 const dbcon = mysql.createPool({
-  connectionLimit : 1000,
-  connectTimeout  : 60 * 60 * 1000,
-  acquireTimeout  : 60 * 60 * 1000,
-  timeout         : 60 * 60 * 1000,
   host: "sql6.freesqldatabase.com",
   user: "sql6440358",
   password: "Amit@tcs11"
