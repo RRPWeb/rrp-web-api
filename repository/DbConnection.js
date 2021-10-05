bec5c2c0a3636267e486fee9f520b4815b4e886b
AmitKSwain <amit.here1@gmail.com>
2021-10-05 13:27:40 +0530

    1 file modified

--- repository/DbConnection.js
+++ repository/DbConnection.js
@@ -1,5 +1,9 @@
 const mysql = require("mysql");
 const dbcon = mysql.createPool({
+  connectionLimit : 1000,
+  connectTimeout  : 60 * 60 * 1000,
+  acquireTimeout  : 60 * 60 * 1000,
+  timeout         : 60 * 60 * 1000,
   host: "sql6.freesqldatabase.com",
   user: "sql6440358",
   password: "Amit@tcs11"
