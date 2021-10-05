c1fcb218ba4673d2bf384e5a10212bf93728b6f9
AmitKSwain <amit.here1@gmail.com>
2021-10-05 21:41:40 +0530

    1 file modified

--- services/UserInfo.service.js
+++ services/UserInfo.service.js
@@ -1,7 +1,7 @@
 const userInfo = require('../repository/UserInfo');
 
 exports.getAllUsers = async () => {
-  let [err,data] = await userInfo.getUsers();
+  let [data,err] = await userInfo.getUsers();
   if(err){
     console.log(err);
     return "Error"
