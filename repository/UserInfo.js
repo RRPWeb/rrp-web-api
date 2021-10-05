833944f136d7365ec537e7ce02e833b2bd8f0aa0
AmitKSwain <amit.here1@gmail.com>
2021-10-05 16:47:25 +0530

    1 file modified

--- repository/UserInfo.js
+++ repository/UserInfo.js
@@ -2,16 +2,18 @@
 const dbcon =require('./DbConnection')
 const query ="SELECT * FROM `lJEpEVWywz`.`USER_INFO`";
 
-const getUsers = ()=>{
-  console.log("Inside getUsers...")
-  dbcon.query(query, (err, rows) => {
-    if (err) {
-      return "Error"
-     
-    } else {
-      return rows[0]
-    }
-  });
-}
 
-module.exports = getUsers
+
+module.exports = {
+  getUsers: function() {
+    console.log("Inside getUsers...")
+    dbcon.query(query, (err, rows) => {
+      if (err) {
+        return "Error"
+       
+      } else {
+        return rows[0]
+      }
+    });
+  }
+}
