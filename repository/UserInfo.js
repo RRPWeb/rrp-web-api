55c2a85a63bfaa311ce3397e2fbc00fa7628831a
AmitKSwain <amit.here1@gmail.com>
2021-10-05 20:46:54 +0530

    1 file modified

--- repository/UserInfo.js
+++ repository/UserInfo.js
@@ -4,16 +4,8 @@ const query ="SELECT * FROM `lJEpEVWywz`.`USER_INFO`";
 
 exports.getUsers = async ()=>{
   console.log("Inside getUsers...")
-  let result= await dbcon.query(query, (err, rows) => {
-    if (err) {
-     console.log(err)
-     return "Error"
-    } else {
-      console.log(rows)
-      return "Success"
-    }
-  });
-
+  const result= await dbcon.query(query)
+  console.log(result)
   return result;
 }
 
