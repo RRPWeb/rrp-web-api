0cd2b890ea9c0cff4390f56bf3ece3947bbf3715
AmitKSwain <amit.here1@gmail.com>
2021-10-05 18:16:07 +0530

    1 file modified

--- services/UserInfo.service.js
+++ services/UserInfo.service.js
@@ -1,8 +1,20 @@
 const userInfo = require('../repository/UserInfo');
 
 const getAllUsers = () => {
-  
-  return "All users "
+  let status,responseData
+  userInfo.getUsers((err, data) => {
+    if (err) {
+        status= '500',
+        responseData= 'Some error occurred while retrieving customers.'
+     
+    } else {
+      status= '200',
+      responseData= data
+     
+    }
+  });
+
+  return {status,responseData}
 };
 
 
