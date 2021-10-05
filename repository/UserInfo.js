e872aec8170fc112696f2fd4772e32a1e57e8215
AmitKSwain <amit.here1@gmail.com>
2021-10-05 13:23:12 +0530

    1 file modified

--- repository/UserInfo.js
+++ repository/UserInfo.js
@@ -3,6 +3,7 @@ const dbcon =require('./DbConnection')
 const query ="SELECT * FROM `sql6440358`.`USER_INFO`";
 
 const getUsers = ()=>{
+  console.log("Inside getUsers...")
   dbcon.query(query, (err, rows) => {
     if (err) {
       console.log(err);
