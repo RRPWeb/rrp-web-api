d0e3124af1a74fb40b24784fd885637f10fa9a96
AmitKSwain <amit.here1@gmail.com>
2021-10-07 17:27:46 +0530

    1 file modified

--- server.js
+++ server.js
@@ -36,9 +36,10 @@ app.use(cors());
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(apiSpec));
 app.use(logger);
-app.get('/', (req, res) => {
-  res.status(200).json(apiSpec);
-});
+app.use('/', apiSpec);
+// app.get('/', (req, res) => {
+//   res.status(200).json(apiSpec);
+// });
 
 app.use('/', routes);
 
