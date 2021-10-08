f392f051805174c1f2c7e36b32df587c2913bb7c
AmitKSwain <amit.here1@gmail.com>
2021-10-08 08:34:09 +0530

    1 file modified

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(helmet());
 app.use(cors());
-app.use(limiter);
+//app.use(limiter);
 app.use(
   OpenApiValidator.middleware({
     apiSpec: './spec.yml'
