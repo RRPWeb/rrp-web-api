2ba09477514ee6d7aecbca1447fb0b76cf41e10a
AmitKSwain <amit.here1@gmail.com>
2021-10-27 12:47:54 +0530

    1 file modified

--- controllers/test.js
+++ controllers/test.js
@@ -1,8 +1,9 @@
 
 const userService = require('../services/UserInfo.service')
 exports.testMessege = async (req,res,next) =>{
-    let response = await userService.getAllUsers()
-    res.status(200).json(response)
-
+    // let response = await userService.getAllUsers()
+    // res.status(200).json(response)
+    const imageUrl = 'https://i.picsum.photos/id/574/300/300.jpg?hmac=D8e1xkNmWFV7sZNtVa8nLzgusD4LendI-0RSMXM86wk';
+        req.pipe(request(imageUrl)).pipe(res);
 }
 
