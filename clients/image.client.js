f699fa8c0c8e8c14a84f0d8827e2750c6b2d9166
AmitKSwain <amit.here1@gmail.com>
2021-10-27 13:30:16 +0530

    2 files modified

--- clients/image.client.js
+++ clients/image.client.js
@@ -1,9 +1,10 @@
 
 var request = require('request');
-exports.getImage = async (req,res) => {
+exports.getImage = async () => {
   
   const imageUrl = 'https://i.picsum.photos/id/574/300/300.jpg?hmac=D8e1xkNmWFV7sZNtVa8nLzgusD4LendI-0RSMXM86wk';
-        req.pipe(request(imageUrl)).pipe(res);
+  
+  return request(imageUrl)
   
   
 };
--- controllers/test.js
+++ controllers/test.js
@@ -7,7 +7,9 @@ exports.testMessege = async (req,res,next) =>{
     // res.status(200).json(response)
     // const imageUrl = 'https://i.picsum.photos/id/574/300/300.jpg?hmac=D8e1xkNmWFV7sZNtVa8nLzgusD4LendI-0RSMXM86wk';
     //     req.pipe(request(imageUrl)).pipe(res);
-    await imageClient.getImage(req,res)
+    let image = await imageClient.getImage()
+
+    req.pipe(image).pipe(res);
 
 }
 
