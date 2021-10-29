8f1b2bfe96e11f3e32c62dc784984bacf4a979c5
AmitKSwain <amit.here1@gmail.com>
2021-10-29 15:51:24 +0530

    1 file modified

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -3,6 +3,6 @@ const authRouter  = express.Router()
 const authController = require('../controllers/auth.controller')
 
 
-authouter.get("/otp/signin",authController.signinOtp)
-otprouter.get("/otp/login",authController.loginOtp)
+authRouter.get("/otp/signin",authController.signinOtp)
+authRouter.get("/otp/login",authController.loginOtp)
 module.exports = authRouter
