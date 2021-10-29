bfb71e793959d247fe69ce66dcc30cb70d032c7f
AmitKSwain <amit.here1@gmail.com>
2021-10-29 17:13:08 +0530

    3 files modified, 1 added

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,3 +1,7 @@
+const jwt = require('jsonwebtoken')
+const tokenList = {}
+const config = require('../config/config.json')
+
 exports.signinOtp =  (req,res,next) =>{
   let response = {
     messege: 'success'
@@ -11,4 +15,22 @@ exports.loginOtp =  (req,res,next) =>{
   }
   res.status(200).json(response)
 
+}
+
+exports.login = (req,res,next) => {
+  const {userPhoneNum,password} = req.body;
+  const accessLevel = 'member'
+  const user = {
+      userPhoneNum ,
+      accessLevel
+    }
+  // do the database authentication here, with user name andpassword combination.
+  const token = jwt.sign(user, config.secret, { expiresIn:config.tokenLife})
+  const refreshToken = jwt.sign(user, config.refreshTokenSecret,{ expiresIn: config.refreshTokenLife})
+  const response = {
+      token,
+      refreshToken
+  }
+  tokenList[refreshToken] = response
+  res.status(200).json(response);
 }
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -5,4 +5,6 @@ const authController = require('../controllers/auth.controller')
 
 authRouter.get("/otp/signin",authController.signinOtp)
 authRouter.get("/otp/login",authController.loginOtp)
+authRouter.post("/login",authController.login)
+authRouter.post("/signin",authController.signin)
 module.exports = authRouter
