ed35a53bca1588c15e0ed6ec4ad08aaf5b7277b2
AmitKSwain <amit.here1@gmail.com>
2021-11-12 02:33:20 +0530

    1 file modified

--- controllers/test.js
+++ controllers/test.js
@@ -2,7 +2,7 @@
 var request = require('request');
 const userService = require('../services/UserInfo.service')
 const imageClient = require('../clients/extFile.client')
-const waClient =require('../clients/whatsapp.client')
+const smsClient =require('../clients/sma.client')
 exports.testMessege = async (req,res,next) =>{
     //image proxy ********************
     // let response = await userService.getAllUsers()
@@ -15,7 +15,7 @@ exports.testMessege = async (req,res,next) =>{
     // let image = await imageClient.getFile(imageUrl)
 
     // req.pipe(image).pipe(res);
-    let msg = await waClient.sendMsg();
+    let msg = await smsClient.sendsms();
 
     res.status(200).json(msg)
 }
