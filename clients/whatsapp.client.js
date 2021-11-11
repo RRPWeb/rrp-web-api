47faebbd53f16d07e88f9426c351bd2eb512c5f3
AmitKSwain <amit.here1@gmail.com>
2021-11-11 12:34:11 +0530

    3 files modified, 1 added

---
+++ clients/whatsapp.client.js
@@ -0,0 +1,12 @@
+const accountSid = "AC03b6aa389f02ab5db03ea074f17e3377";
+const authToken = "b4be6d57ed3e7bb87346e1324919be40";
+const client = require('twilio')(accountSid, authToken);
+
+exports.sendWaMsg= ()=>{
+  client.messages
+      .create({
+         body: 'Hello there from twilio!',
+         to: 'whatsapp:+919094763431'
+       })
+      .then(message => console.log(message.sid));
+}
--- controllers/test.js
+++ controllers/test.js
@@ -2,15 +2,21 @@
 var request = require('request');
 const userService = require('../services/UserInfo.service')
 const imageClient = require('../clients/extFile.client')
+const waClient =require('../clients/whatsapp.client')
 exports.testMessege = async (req,res,next) =>{
+    //image proxy ********************
     // let response = await userService.getAllUsers()
     // res.status(200).json(response)
     // const imageUrl = 'https://i.picsum.photos/id/574/300/300.jpg?hmac=D8e1xkNmWFV7sZNtVa8nLzgusD4LendI-0RSMXM86wk';
     //     req.pipe(request(imageUrl)).pipe(res);
-    const imageUrl = 'https://legislative.gov.in/sites/default/files/coi-4March2016.pdf';
-    let image = await imageClient.getFile(imageUrl)
 
-    req.pipe(image).pipe(res);
+    //pdf proxy ***********************
+    // const imageUrl = 'https://legislative.gov.in/sites/default/files/coi-4March2016.pdf';
+    // let image = await imageClient.getFile(imageUrl)
+
+    // req.pipe(image).pipe(res);
+    await waClient.sendWaMsg();
+
 
 }
 
