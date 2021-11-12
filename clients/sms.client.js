d85272af0c73d2c3330cc33fc8ab1f7a48806241
AmitKSwain <amit.here1@gmail.com>
2021-11-12 11:53:53 +0530

    3 files modified

--- clients/sms.client.js
+++ clients/sms.client.js
@@ -2,13 +2,12 @@ const accountSid = "AC03b6aa389f02ab5db03ea074f17e3377";
 const authToken = "f1fb04b9d153dec126a506f56f84c5e7";
 const client = require('twilio')(accountSid, authToken);
 
-exports.sendsms= async ()=>{
-  const phNum ='+918820904591'
-  const smstext = "Your login OTP in RTR PORTAL for the day is : "
-  console.log("In sms client")
+exports.sendsms= async (msg,phNum)=>{
+  phNum ='+918820904591'
+  
   client.messages
       .create({
-        body: smstext,
+        body: msg,
         messagingServiceSid: "MG95bc3e1d2a35bf561e26a1b126986904",
         to: phNum,
        })
--- clients/whatsapp.client.js
+++ clients/whatsapp.client.js
@@ -2,13 +2,14 @@ const accountSid = 'AC03b6aa389f02ab5db03ea074f17e3377';
 const authToken = 'f1fb04b9d153dec126a506f56f84c5e7'; 
 const client = require('twilio')(accountSid, authToken);
 
-exports.sendMsg= async ()=>{
+exports.sendMsg= async (msg,phNum)=>{
   console.log("in whatsapp client")
+  const waNum ='whatsapp:'+phNum
   client.messages
       .create({
-         body: 'Hello there from twilio RRP!',
+         body: msg,
          from: 'whatsapp:+14155238886',  
-         to: 'whatsapp:+918820904591'
+         to: waNum
        })
       .then(message => console.log(message.sid)).done();
       return 'Success'
