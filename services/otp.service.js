1b204422a2588c87b16aa8357628e20f274bc01e
AmitKSwain <amit.here1@gmail.com>
2021-11-16 12:45:27 +0530

    2 files modified

--- services/otp.service.js
+++ services/otp.service.js
@@ -1,5 +1,5 @@
 
-const otpRepo = require('../services/otp.repo')
+const otpRepo = require('../repositories/otp.repo')
 exports.createOtp = async (phoneNumber ,clientIp,validity,type)=> {
   const otp = Math.floor(1000000 + Math.random() * 9000000)
   const result = await otpRepo.createOtp(phoneNumber,clientIp,otp,validity,type)
