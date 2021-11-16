ea04dcc7ae6e2acb876581a137a66b98cac22490
AmitKSwain <amit.here1@gmail.com>
2021-11-16 17:23:11 +0530

    1 file modified

--- services/signup.service.js
+++ services/signup.service.js
@@ -5,13 +5,19 @@ const userRepo = require('../repositories/user.repo')
 exports.signup = async (phoneNumber,voterId,name,clientIp,otp,type)=> {
   
   const isOtpValid = await otpService.verifyOtp(phoneNumber,clientIp,otp,type)
-  let result = isOtpValid // just for teting 
-  //call the signup repo or user repo to save user data 
-  const secondsSinceEpoch = Math.round(Date.now() / 1000)
-  const prefix = Math.floor(100 + Math.random() * 900)
-  const postfix = Math.floor(100 + Math.random() * 900)
-  const userId = ''+prefix+secondsSinceEpoch+postfix
-  await userRepo.createUser(userId,phoneNumber,voterId,name)
   
-  return result
+  if(isOtpValid){
+    const secondsSinceEpoch = Math.round(Date.now() / 1000)
+    const prefix = Math.floor(100 + Math.random() * 900)
+    const postfix = Math.floor(100 + Math.random() * 900)
+    const userId = ''+prefix+secondsSinceEpoch+postfix
+    let result = await userRepo.createUser(userId,phoneNumber,voterId,name)
+    if(!result.error){
+      return {messege: 'User is registered sucessfully'}
+    }
+    return {error: 'Error occured while creating user'}
+  }
+  
+  
+  return {error: 'Entered OTP is invalid or expired'}
 }
