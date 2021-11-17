a5f02ac82f03e54b390c51b2f4d2b464dc4d9713
AmitKSwain <amit.here1@gmail.com>
2021-11-17 20:17:52 +0530

    1 file modified

--- repositories/user.repo.js
+++ repositories/user.repo.js
@@ -2,11 +2,9 @@
 const dbHelper =require('./dbhelper')
 
 exports.getUsers = async () => {
-  const sql ="SELECT * FROM `lJEpEVWywz`.`USER_PROFILE`";
+  const sql ="SELECT * as FROM `lJEpEVWywz`.`USER_PROFILE`";
   try{
       let response = await dbHelper.execDql(sql)
-      //response = JSON.parse(JSON.stringify(response))
-     
       return {data: response}
     }catch(err){
       
