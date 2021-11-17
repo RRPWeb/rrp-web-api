15e5780c465c4c71808351acf1e92a1b6ce845cd
AmitKSwain <amit.here1@gmail.com>
2021-11-17 16:28:11 +0530

    1 file modified

--- services/login.service.js
+++ services/login.service.js
@@ -1,21 +1,22 @@
+const otpService = require('./otp.service');
+const userRepo = require('../repositories/user.repo');
 
-const otpService = require('./otp.service')
-const userRepo = require('../repositories/user.repo')
+exports.login = async (phoneNumber, clientIp, otp, type) => {
+  const isOtpValid = await otpService.verifyOtp(
+    phoneNumber,
+    clientIp,
+    otp,
+    type
+  );
 
-exports.login = async (phoneNumber,clientIp,otp,type)=> {
-  
-  const isOtpValid = await otpService.verifyOtp(phoneNumber,clientIp,otp,type)
-  
-  if(isOtpValid){
-    
-    let result = await userRepo.getUserByPhNum(phoneNumber)
-    if(!result.error){
-      return result.data[0]
+  if (isOtpValid) {
+    let result = await userRepo.getUserByPhNum(phoneNumber);
+    if (!result.error) {
+      return { userId: result.data[0].USER_ID, status: result.data[0].STATUS };
     }
-    
-    return {error:'Some problem occured during login'}
+
+    return { error: 'Some problem occured during login' };
   }
-  
-  
-  return {error: 'Entered OTP is invalid or expired'}
-}
+
+  return { error: 'Entered OTP is invalid or expired' };
+};
