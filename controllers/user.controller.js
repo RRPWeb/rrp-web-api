3d174383fdec8f6e713d486a307109bae79ba092
AmitKSwain <amit.here1@gmail.com>
2021-11-17 20:08:31 +0530

    1 file modified

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -2,5 +2,7 @@
 const userService = require('../services/user.service');
 exports.userProfiles = (req,res,next) =>{
   const response = userService.getAllUsers()
+  console.log(' in controller' )
+  console.log(response)
   res.status(200).json(response)
 }
