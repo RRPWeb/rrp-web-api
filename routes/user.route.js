89b49e239e4efde1d78db1b805c5157869f2a7c3
AmitKSwain <amit.here1@gmail.com>
2021-11-17 23:49:16 +0530

    1 file modified

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const userRouter  = express.Router()
 const userController = require('../controllers/user.controller')
-const authValidator = require('./middlewares/authvalidator');
+const authValidator = require('../middlewares/authvalidator');
 
 
 userRouter.get("/",authValidator,userController.userProfiles)
