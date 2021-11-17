793fa338356e0f5fe8090da52aaa7ffbf21ad8e5
AmitKSwain <amit.here1@gmail.com>
2021-11-17 14:00:32 +0530

    1 file modified

--- services/otp.service.js
+++ services/otp.service.js
@@ -15,7 +15,7 @@ const saveAndSendOtp= async (phoneNumber ,clientIp,validity,type)=>{
 exports.createOtp = async (phoneNumber ,clientIp,validity,type)=> {
   const userInfo= await userRepo.getUserByPhNum(phoneNumber)
   console.log(userInfo.data)
-  if((userInfo.data && type==='SIGNUP')||(!userInfo.data && type==='LOGIN')){
+  if((!userInfo.data && type==='SIGNUP')||(userInfo.data && type==='LOGIN')){
     return await saveAndSendOtp(phoneNumber ,clientIp,validity,type)
   }else{
     return {error : 'Operation not allowed'}
