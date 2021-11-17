a58849aebca34afb56d31c9e17d80ce8628a1ff6
AmitKSwain <amit.here1@gmail.com>
2021-11-17 20:15:33 +0530

    1 file modified

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,6 +1,6 @@
 
 const userService = require('../services/user.service');
-exports.userProfiles = (req,res,next) =>{
+exports.userProfiles =async  (req,res,next) =>{
   const response = await userService.getAllUsers()
 
   res.status(200).json(response)
