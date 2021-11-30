b633da47730534d022ee09ff39aef736e81802c9
Amit Kumar Swain <amit.here1@gmail.com>
2021-11-30 17:24:40 +0530

    route change for refresh token

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,10 +1,10 @@
-const express = require('express')
-const authRouter  = express.Router()
-const authController = require('../controllers/auth.controller')
+const express = require("express");
+const authRouter = express.Router();
+const authController = require("../controllers/auth.controller");
 
-
-authRouter.get("/otp/signin",authController.signinOtp)
-authRouter.get("/otp/login",authController.loginOtp)
-authRouter.post("/login",authController.login)
-authRouter.post("/signin",authController.signin)
-module.exports = authRouter
+authRouter.get("/otp/signin", authController.signinOtp);
+authRouter.get("/otp/login", authController.loginOtp);
+authRouter.post("/login", authController.login);
+authRouter.post("/signin", authController.signin);
+authRouter.post("/refreshToken", authController.refreshToken);
+module.exports = authRouter;
