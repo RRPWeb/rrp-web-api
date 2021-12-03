6ea27fb874c5167821a0b498872812de0bbf48e1
Amit Kumar Swain <amit.here1@gmail.com>
2021-12-03 15:55:37 +0530

    refreshtoken service layer added

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,45 +1,72 @@
+const config = require("../configs/config.json");
+const otpSevice = require("../services/otp.service");
+const signupSevice = require("../services/signup.service");
+const loginSevice = require("../services/login.service");
+var get_ip = require("ipware")().get_ip;
 
-const config = require('../configs/config.json')
-const otpSevice = require('../services/otp.service')
-const signupSevice = require('../services/signup.service')
-const loginSevice = require('../services/login.service')
-var get_ip = require('ipware')().get_ip;
+exports.signinOtp = async (req, res, next) => {
+  const clientIp = get_ip(req).clientIp;
 
-exports.signinOtp = async (req,res,next) =>{
-  const  clientIp = get_ip(req).clientIp
+  const phNum = req.query.phoneNumber;
 
-  const phNum = req.query.phoneNumber
+  let response = await otpSevice.createOtp(
+    phNum,
+    clientIp,
+    config.tokenLife,
+    "SIGNUP"
+  );
 
-  let response = await otpSevice.createOtp(phNum,clientIp,config.tokenLife,'SIGNUP')
-
-  res.status(200).json(response)
-
-}
-exports.loginOtp = async (req,res,next) =>{
-  const  clientIp = get_ip(req).clientIp
-
-  const phNum = req.query.phoneNumber
-  let response = await otpSevice.createOtp(phNum,clientIp,config.tokenLife,'LOGIN')
-  res.status(200).json(response)
-
-}
-
-exports.login = async (req,res,next) => {
-  const  clientIp = get_ip(req).clientIp
-  const {userPhoneNum,otp} = req.body;
-  
-  const response = await loginSevice.login(userPhoneNum,clientIp,otp,'LOGIN',)
   res.status(200).json(response);
-}
+};
+exports.loginOtp = async (req, res, next) => {
+  const clientIp = get_ip(req).clientIp;
 
-exports.signin = async (req,res,next) => {
-  const {name,userPhoneNum,voterId,otp,ageQualifier,isGovtEmployee} = req.body;
-  const  clientIp = get_ip(req).clientIp
-  const response = await signupSevice.signup(userPhoneNum,voterId,name,clientIp,otp,'SIGNUP')
+  const phNum = req.query.phoneNumber;
+  let response = await otpSevice.createOtp(
+    phNum,
+    clientIp,
+    config.tokenLife,
+    "LOGIN"
+  );
+  res.status(200).json(response);
+};
+
+exports.login = async (req, res, next) => {
+  const clientIp = get_ip(req).clientIp;
+  const { userPhoneNum, otp } = req.body;
+
+  const response = await loginSevice.login(
+    userPhoneNum,
+    clientIp,
+    otp,
+    "LOGIN"
+  );
+  res.status(200).json(response);
+};
+
+exports.signin = async (req, res, next) => {
+  const {
+    name,
+    userPhoneNum,
+    voterId,
+    otp,
+    ageQualifier,
+    isGovtEmployee
+  } = req.body;
+  const clientIp = get_ip(req).clientIp;
+  const response = await signupSevice.signup(
+    userPhoneNum,
+    voterId,
+    name,
+    clientIp,
+    otp,
+    "SIGNUP"
+  );
   res.status(201).json(response);
-}
+};
 
-exports.refreshToken = async (req,res,next) => {
-  let response = {messege : 'Success'}
+exports.refreshToken = (req, res, next) => {
+  let response = loginSevice.refreshToken(req.body.refreshToken);
+
   res.status(200).json(response);
-}
+};
--- services/login.service.js
+++ services/login.service.js
@@ -1,8 +1,8 @@
-const jwt = require('jsonwebtoken')
-const config = require('../configs/config.json')
-const otpService = require('./otp.service');
-const userRepo = require('../repositories/user.repo');
-let tokenList = []
+const jwt = require("jsonwebtoken");
+const config = require("../configs/config.json");
+const otpService = require("./otp.service");
+const userRepo = require("../repositories/user.repo");
+let tokenList = [];
 exports.login = async (phoneNumber, clientIp, otp, type) => {
   const isOtpValid = await otpService.verifyOtp(
     phoneNumber,
@@ -16,27 +16,54 @@ exports.login = async (phoneNumber, clientIp, otp, type) => {
     if (!result.error) {
       let user = {
         userId: result.data[0].USER_ID,
-        status: result.data[0].STATUS,
+        status: result.data[0].STATUS
       };
 
       const token = jwt.sign(user, config.secret, {
-        expiresIn: config.tokenLife,
+        expiresIn: config.tokenLife
       });
       const refreshToken = jwt.sign(user, config.refreshTokenSecret, {
-        expiresIn: config.refreshTokenLife,
+        expiresIn: config.refreshTokenLife
       });
       let response = {
         token,
-        tokenLife:config.tokenLife,
+        tokenLife: config.tokenLife,
         refreshToken,
-        refreshTokenLife:config.refreshTokenLife
+        refreshTokenLife: config.refreshTokenLife
       };
-      tokenList[refreshToken] = response;
-      return response
+      tokenList.push(refreshToken);
+      return response;
     }
 
-    return { error: 'Some problem occured during login' };
+    return { error: "Some problem occured during login" };
   }
 
-  return { error: 'Entered OTP is invalid or expired' };
+  return { error: "Entered OTP is invalid or expired" };
+};
+
+exports.refreshToken = (refreshTOken) => {
+  if (refreshTOken in tokenList) {
+    let user = {
+      userId: "78996433224456",
+      status: "ACTIVE"
+    };
+    let response;
+    const token = jwt.sign(user, config.secret, {
+      expiresIn: config.tokenLife
+    });
+    const refreshTokenNew = jwt.sign(user, config.refreshTokenSecret, {
+      expiresIn: config.refreshTokenLife
+    });
+    response = {
+      token,
+      tokenLife: config.tokenLife,
+      refreshToken: refreshTokenNew,
+      refreshTokenLife: config.refreshTokenLife
+    };
+    tokenList[refreshTokenNew] = response;
+    tokenList.filter((item) => item !== refreshTOken);
+    return response;
+  } else {
+    return { err: "TOken not valid" };
+  }
 };
